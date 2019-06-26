6d9c282f9c9508ef1f7a9d6eed4325b0bbba96f3
Emi Ko <emi8emi@gmail.com>
2019-06-26 16:53:50 -0700

    Add OwlCarousel and styling

--- js/script.js
+++ js/script.js
@@ -3,3 +3,17 @@ $(window).on('load', () => {
   $("#status").fadeOut();
   $("#preloader").delay(350).fadeOut('slow');
 });
+// Team
+//$(document).ready(function(){...} shortened to $(function(){...})
+$(function(){
+  $("#team-members").owlCarousel({
+    items: 2,
+    autoplay: true,
+    smartSpeed: 700,
+    loop: true,
+    autoplayHoverPause:true,
+    nav: true,
+    dots: false,
+    navText:['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>']
+  });
+});

c752255a5fd5673adbbb7c30d5c464967c2774df
Emi Ko <emi8emi@gmail.com>
2019-06-27 14:20:18 -0700

    Add Waypoints jquery extension to activate Progress bars on scroll

--- js/script.js
+++ js/script.js
@@ -5,15 +5,29 @@ $(window).on('load', () => {
 });
 // Team
 //$(document).ready(function(){...} shortened to $(function(){...})
-$(function(){
+$(function() {
   $("#team-members").owlCarousel({
     items: 2,
     autoplay: true,
     smartSpeed: 700,
     loop: true,
-    autoplayHoverPause:true,
+    autoplayHoverPause: true,
     nav: true,
     dots: false,
-    navText:['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>']
+    navText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>']
+  });
+});
+
+// Progress Bars
+$(function() {
+  $("#progress-elements").waypoint(function() {
+    $(".progress-bar").each(function() {
+      $(this).animate({
+        width: $(this).attr("aria-valuenow") + "%"
+      }, 1000);
+    });
+    this.destroy();
+  }, {
+    offset: 'bottom-in-view'
   });
 });
